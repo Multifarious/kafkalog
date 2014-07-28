3a221a6b47fe655d8b7b196a32c085515c2336dc
b <b@b3k.us>
2014-07-28 16:52:43 -0700

    Add logging and a timeout for buffer polling

--- src/main/java/io/ifar/kafkalog/kafka/LogProducer.java
+++ src/main/java/io/ifar/kafkalog/kafka/LogProducer.java
@@ -1,17 +1,16 @@
 package io.ifar.kafkalog.kafka;
 
+import com.codahale.metrics.health.HealthCheck.Result;
 import io.dropwizard.lifecycle.Managed;
 import kafka.javaapi.producer.Producer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
 
 public class LogProducer implements Managed {
     static final Logger LOG = LoggerFactory.getLogger(LogProducer.class);
+    static final long BUFFER_POLL_TIMEOUT = 1000L;
 
     private final ExecutorService executor = Executors.newSingleThreadExecutor();
     private final BlockingQueue<String> lineBuffer;
@@ -26,13 +25,23 @@ public class LogProducer implements Managed {
         this.producer = producer;
     }
 
+    public Result healthCheck() {
+        if (runnable == null || runnable.isCancelled() || runnable.isDone()) {
+            return Result.unhealthy("Producer task isn't running");
+        }
+
+        return Result.healthy();
+    }
+
     @Override
     public void start() throws Exception {
+        LOG.info("Submitting producer task to executor");
         runnable = executor.submit(new LogProducerRunnable());
     }
 
     @Override
     public void stop() throws Exception {
+        LOG.info("Cancelling producer task");
         runnable.cancel(true);
     }
 
@@ -41,8 +50,14 @@ public class LogProducer implements Managed {
         @SuppressWarnings("unchecked")
         public void run() {
             while (true) {
-                String line = lineBuffer.poll();
-                producer.send(messageFactory.create(line));
+                String line = null;
+                try {
+                    line = lineBuffer.poll(BUFFER_POLL_TIMEOUT, TimeUnit.MILLISECONDS);
+                    LOG.debug("Sending buffered data: {}", line);
+                    producer.send(messageFactory.create(line));
+                } catch (InterruptedException e) {
+                    LOG.trace("Buffer poll timeout expired, continuing.");
+                }
 
                 if (Thread.currentThread().isInterrupted()) {
                     return;
